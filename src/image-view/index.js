67fc598e0364fbf799bbc7337c996a0d840b93e7
孙健 <jian.sun@ymm56.com>
2019-01-14 19:57:30 +0800

    u

--- src/image-view/index.js
+++ src/image-view/index.js
@@ -2,6 +2,8 @@
 import directive from './src/directive';
 import service from './src/index';
 import './index.scss'
+import ymm from 'ymm-design'
+console.log(ymm)
 
 service.install = function(Vue) {
   Vue.use(directive);
