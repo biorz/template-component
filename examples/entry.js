e0433b01f8ed556ec22b44d227778ee56fefdc1f
孙健 <jian.sun@ymm56.com>
2019-01-14 19:38:09 +0800

    u

--- examples/entry.js
+++ examples/entry.js
@@ -3,7 +3,9 @@
 import Vue from 'vue';
 import entry from './app';
 import Ui from 'main/index.js';
+import DemoBlock from './components/demo-block'
 Vue.use(Ui);
+Vue.component('demo-block', DemoBlock);
 
 new Vue({ // eslint-disable-line
   name: 'app',
