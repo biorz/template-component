509135c296ef21a593995f9af1152ae4aa9b215d
孙健 <jian.sun@ymm56.com>
2019-01-22 18:59:37 +0800

    u

--- examples/app.vue
+++ examples/app.vue
@@ -292,52 +292,50 @@
     .content {
       padding-top: 50px;
 
-      > {
-        h3 {
-          margin: 55px 0 20px;
+      h3 {
+        margin: 55px 0 20px;
+      }
+
+      table {
+        border-collapse: collapse;
+        width: 100%;
+        background-color: #fff;
+        font-size: 14px;
+        margin-bottom: 45px;
+        line-height: 1.5em;
+
+        strong {
+          font-weight: normal;
         }
 
-        table {
-          border-collapse: collapse;
-          width: 100%;
-          background-color: #fff;
-          font-size: 14px;
-          margin-bottom: 45px;
-          line-height: 1.5em;
-
-          strong {
-            font-weight: normal;
-          }
-
-          td, th {
-            border-bottom: 1px solid #d8d8d8;
-            padding: 15px;
-            max-width: 250px;
-          }
-
-          th {
-            text-align: left;
-            white-space: nowrap;
-            color: #666;
-            font-weight: normal;
-          }
-
-          td {
-            color: #333;
-          }
-
-          th:first-child, td:first-child {
-            padding-left: 10px;
-          }
+        td, th {
+          border-bottom: 1px solid #d8d8d8;
+          padding: 15px;
+          max-width: 250px;
         }
 
-        ul:not(.timeline) {
-          margin: 10px 0;
-          padding: 0 0 0 20px;
-          font-size: 14px;
-          color: #5e6d82;
-          line-height: 2em;
+        th {
+          text-align: left;
+          white-space: nowrap;
+          color: #666;
+          font-weight: normal;
         }
+
+        td {
+          color: #333;
+        }
+
+        th:first-child, td:first-child {
+          padding-left: 10px;
+        }
+      }
+
+      ul:not(.timeline) {
+        margin: 10px 0;
+        padding: 0 0 0 20px;
+        font-size: 14px;
+        color: #5e6d82;
+        line-height: 2em;
       }
     }
 
@@ -390,7 +388,7 @@
       .content {
         padding-top: 0;
       }
-      .content > table {
+      .content table {
         overflow: auto;
         display: block;
       }
