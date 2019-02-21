d9f1efa06c34ae3e0e34a18da9c51b6a8b5c083f
孙健 <jian.sun@ymm56.com>
2019-02-21 10:43:08 +0800

    optimize

--- build/webpack.demo.js
+++ build/webpack.demo.js
@@ -3,36 +3,16 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const md = require('markdown-it')();
-const slugify = require('transliteration').slugify;
-
-const striptags = require('./strip-tags');
 const config = require('./config');
 
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = process.env.NODE_ENV === 'development';
-const isPlay = !!process.env.PLAY_ENV;
-
-function convert(str) {
-  str = str.replace(/(&#x)(\w{4});/gi, function($0) {
-    return String.fromCharCode(parseInt(encodeURIComponent($0).replace(/(%26%23x)(\w{4})(%3B)/g, '$2'), 16));
-  });
-  return str;
-}
-
-function wrap(render) {
-  return function() {
-    return render.apply(this, arguments)
-      .replace('<code v-pre class="', '<code class="hljs ')
-      .replace('<code>', '<code class="hljs">');
-  };
-}
 
 const webpackConfig = {
   entry: isProd ? {
     docs: './examples/entry.js',
     'ymm-design': './src/index.js'
-  } : (isPlay ? './examples/play.js' : './examples/entry.js'),
+  } : ('./examples/play.js'),
   output: {
     path: path.resolve(process.cwd(), './examples/ymm-design/'),
     publicPath: process.env.CI_ENV || '',
@@ -69,57 +49,6 @@ const webpackConfig = {
         include: process.cwd(),
         exclude: config.jsexclude,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.md$/,
-        loader: 'vue-markdown-loader',
-        options: {
-          use: [
-            [require('markdown-it-anchor'), {
-              level: 2,
-              slugify: slugify,
-              permalink: true,
-              permalinkBefore: true
-            }],
-            [require('markdown-it-container'), 'demo', {
-              validate: function(params) {
-                return params.trim().match(/^demo\s*(.*)$/);
-              },
-
-              render: function(tokens, idx) {
-                var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
-                if (tokens[idx].nesting === 1) {
-                  var description = (m && m.length > 1) ? m[1] : '';
-                  var content = tokens[idx + 1].content;
-                  var html = convert(striptags.strip(content, ['script', 'style'])).replace(/(<[^>]*)=""(?=.*>)/g, '$1');
-                  var script = striptags.fetch(content, 'script');
-                  var style = striptags.fetch(content, 'style');
-                  var jsfiddle = { html: html, script: script, style: style };
-                  var descriptionHTML = description
-                    ? md.render(description)
-                    : '';
-
-                  jsfiddle = md.utils.escapeHtml(JSON.stringify(jsfiddle));
-
-                  return `<demo-block class="demo-box" :jsfiddle="${jsfiddle}">
-                            <div class="source" slot="source">${html}</div>
-                            ${descriptionHTML}
-                            <div class="highlight" slot="highlight">`;
-                }
-                return '</div></demo-block>\n';
-              }
-            }],
-            [require('markdown-it-container'), 'tip'],
-            [require('markdown-it-container'), 'warning']
-          ],
-          preprocess: function(MarkdownIt, source) {
-            MarkdownIt.renderer.rules.table_open = function() {
-              return '<table class="table">';
-            };
-            MarkdownIt.renderer.rules.fence = wrap(MarkdownIt.renderer.rules.fence);
-            return source;
-          }
-        }
       },
       {
         test: /\.json$/,
@@ -162,8 +91,7 @@ const webpackConfig = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './examples/index.tpl',
-      filename: './index.html',
-      favicon: './examples/favicon.ico'
+      filename: './index.html'
     }),
     new ProgressBarPlugin(),
     new webpack.LoaderOptionsPlugin({
